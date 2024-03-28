d5ac6f080b5e5389f34558b01a6bcf2d94bfaf6c
Yara-R <yara.rodrigues.inacio@gmail.com>
2024-03-28 10:04:48 -0300

    Loop para client

--- client/client.py
+++ client/client.py
@@ -2,10 +2,16 @@ from utils_client import *
 
 serverName = '127.0.0.1'
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-sentece = input('Input lowercase sentece:')
-clientSocket.send(sentece.encode())
-modifiedSentece = clientSocket.recv(1024)
-print('From Server: ', modifiedSentece.decode())
-clientSocket.close()
+
+while True:
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName, serverPort))
+    sentence = input('Input lowercase sentece: ')
+
+    if sentence == 'exit':
+        break
+
+    clientSocket.send(sentence.encode())
+    modifiedSentece = clientSocket.recv(1024)
+    print('From Server: ', modifiedSentece.decode())
+    clientSocket.close()
